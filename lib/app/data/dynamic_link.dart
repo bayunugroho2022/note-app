1adac9032e528ac4959296f8303c822d4dc7315b
bayunugroho404 <baay404@gmail.com>
2021-10-31 16:32:17 +0700

    chrone: drop create deeplink

--- lib/app/data/dynamic_link.dart
+++ lib/app/data/dynamic_link.dart
@@ -1,4 +1,54 @@
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+
 /**
  * Created by Bayu Nugroho
  * Copyright (c) 2021 . All rights reserved.
  */
+class DynamicLinkService {
+
+  Future<void> initDynamicLinks() async {
+    //on background
+    FirebaseDynamicLinks.instance.onLink(
+        onSuccess: (PendingDynamicLinkData? dynamicLink) async {
+          final Uri? deepLink = dynamicLink?.link;
+
+          if (deepLink != null) {
+            print(deepLink.queryParameters.values.first);
+          }
+        }, onError: (OnLinkErrorException e) async {
+      print('onLinkError');
+      print(e.message);
+    });
+
+    //on foreground
+    final PendingDynamicLinkData? data =
+    await FirebaseDynamicLinks.instance.getInitialLink();
+    final Uri? deepLink = data?.link;
+
+    if (deepLink != null) {
+      print(deepLink.queryParameters.values.first);
+    }
+  }
+
+  Future<Uri> createDynamicLink(String id) async {
+    final DynamicLinkParameters parameters = DynamicLinkParameters(
+      uriPrefix: 'https://bayunugroho404.page.link',
+      link: Uri.parse('https://www.evoqys.in/?aa=$id'),
+      androidParameters: AndroidParameters(
+        packageName: 'com.bayunugroho404.noteapp.noteapp',
+        minimumVersion: 1,
+      ),
+      iosParameters: IosParameters(
+        bundleId: 'your_ios_bundle_identifier',
+        minimumVersion: '1',
+        appStoreId: 'your_app_store_id',
+      ),
+    );
+    // var dynamicUrl = await parameters.buildUrl();
+    final ShortDynamicLink shortLink = await parameters.buildShortLink();
+    var dynamicUrl = shortLink.shortUrl;
+
+    return dynamicUrl;
+  }
+
+}
--- lib/app/modules/intro/controllers/intro_controller.dart
+++ lib/app/modules/intro/controllers/intro_controller.dart
@@ -1,17 +1,21 @@
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:noteapp/app/data/dynamic_link.dart';
 import 'package:noteapp/app/data/secure_storage.dart';
 import 'package:noteapp/app/helpers/tes.dart';
 import 'package:noteapp/app/routes/app_pages.dart';
 import 'package:noteapp/app/widgets/widget_indicator.dart';
+import 'package:share/share.dart';
 
 class IntroController extends GetxController {
   PageController? pageController;
   final currentIndex = 0.obs;
   final secureStorage = SecureStorage();
-
+  late DynamicLinkService dynamicLinkService;
   @override
   void onInit() {
+    dynamicLinkService = DynamicLinkService();
     pageController = PageController(
         initialPage: 0
     );
@@ -23,6 +27,7 @@ class IntroController extends GetxController {
   void onReady() {
     FirebaseConfig.initialMessageHandler();
     FirebaseConfig.onMessageOpenApp();
+    dynamicLinkService.initDynamicLinks();
     super.onReady();
   }
 
@@ -49,5 +54,15 @@ class IntroController extends GetxController {
     if(isLogin){
       Get.offAndToNamed(Routes.HOME);
     }
+  }
+
+
+  void createDynamicLink()async{
+    await dynamicLinkService.createDynamicLink("testestes").then((value) {
+      print('hasil $value');
+      // Share.share(value.toString());
+    }).catchError((onError){
+      print('${onError}');
+    });
   }
 }
